cceb2f3cd9079a40aadece2b311c4c0f108ff211
Muhammed Fatih Öztel <74593165+muhammed-oztel@users.noreply.github.com>
2022-07-28 14:41:54 +0300

    Login initial page added

---
+++ src/pages/LoginPage.svelte
@@ -0,0 +1,65 @@
+<script>
+  import Button from "@smui/button";
+  import Textfield from "@smui/textfield";
+  import Icon from "@smui/textfield/icon";
+  let userEmail = "";
+  let userPassword = "";
+</script>
+
+<div class="container-fluid">
+  <div class="row justify-content-center ">
+    <div class="col-6">
+      <img
+        src="https://images.unsplash.com/photo-1534196511436-921a4e99f297"
+        class="vh-100 img-fluid"
+        alt="login-image"
+      />
+    </div>
+    <div class="col-6">
+      <div class="col mt-5 text-center">
+        <h1 class="pt-5">
+          Menthor'a <br />Hoşgeldiniz!
+        </h1>
+        <h2 class="pt-5">Giriş Yap</h2>
+      </div>
+      <div class="row text-center">
+        <div class="col">
+          <div class="mb-2">
+            <Textfield
+              class=""
+              variant="outlined"
+              bind:value={userEmail}
+              label="Mail"
+            >
+              <Icon class="material-icons" slot="leadingIcon">mail</Icon>
+            </Textfield>
+          </div>
+          <div>
+            <Textfield
+              class=""
+              variant="outlined"
+              bind:value={userPassword}
+              label="Şifre"
+            >
+              <Icon class="material-icons" slot="leadingIcon">password</Icon>
+            </Textfield>
+          </div>
+        </div>
+      </div>
+
+      <div class="text-center mt-3 ms-6">
+        <Button color="primary" variant="raised">Giriş Yap</Button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+
+.ms-6 {
+
+  margin-left: 140px;
+
+}
+
+</style>
