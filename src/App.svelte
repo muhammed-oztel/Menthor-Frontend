621502637b4d9ab424f126133d28cf41acbecf59
melihcelik <melihcelik0909@gmail.com>
2022-07-29 18:07:24 +0300

    Route işlemleri paketi yüklendi

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,16 @@
 <script>
-
-import Router from 'svelte-spa-router';
-import RegisterPage from './pages/RegisterPage.svelte';
-  import LandingPage  from './pages/LandingPage.svelte';
-  import LoginPage  from './pages/LoginPage.svelte';
-import Profile from './pages/Profile.svelte';
-  let routes={
-    '/' : LandingPage,
-  '/RegisterPage': RegisterPage,
-  '/LoginPage' : LoginPage,
-  '/Profile': Profile
-}
+  import Router from "svelte-spa-router";
+  import RegisterPage from "./pages/RegisterPage.svelte";
+  import LandingPage from "./pages/LandingPage.svelte";
+  import LoginPage from "./pages/LoginPage.svelte";
+  import Profile from "./pages/Profile.svelte";
+  let routes = {
+    "/": LandingPage,
+    "/RegisterPage": RegisterPage,
+    "/LoginPage": LoginPage,
+    "/Profile": Profile,
+  };
 </script>
-
 
 <!-- markup (zero or more items) goes here -->
 
