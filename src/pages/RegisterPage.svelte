e89805e5da9bf588ce099c59f35e6f905a928638
melihcelik <melihcelik0909@gmail.com>
2022-07-29 16:56:12 +0300

    Register Page Validasyonu ve Succes Bildirimi

--- src/pages/RegisterPage.svelte
+++ src/pages/RegisterPage.svelte
@@ -1,23 +1,128 @@
 <script>
-  let name = "";
-  let surname = "";
-  let email = "";
-  let phone = "";
-  let birth = "";
-  let password = "";
   import Textfield from "@smui/textfield";
   import Icon from "@smui/textfield/icon";
   import Button, { Label } from "@smui/button";
   import SideImage from "../components/SideImage.svelte";
   import SveltyPicker from "svelty-picker";
+  import * as yup from "yup";
+  import toast, { Toaster } from "svelte-french-toast";
 
-  function submitForm(event) {
-    event.preventDefault();
-    console.log(name + " " + surname + " " + email + " " + password + " ");
+  let user = {
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    birthdate: "",
+    password: "",
+  };
+  let errors = {
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    birthdate: "",
+    password: "",
+  };
+
+  const tr = {
+    days: [
+      "Pazar",
+      "Pazartesi",
+      "Salı",
+      "Çarşamba",
+      "Perşembe",
+      "Cuma",
+      "Cumartesi",
+    ],
+    months: [
+      "Ocak",
+      "Şubat",
+      "Mart",
+      "Nisan",
+      "Mayıs",
+      "Haziran",
+      "Temmuz",
+      "Ağustos",
+      "Eylül",
+      "Ekim",
+      "Kasım",
+      "Aralık",
+    ],
+
+    daysShort: ["Paz", "Pzt", "Sal", "Çarş", "Perş", "Cum", "Cmt", "Paz"],
+    daysMin: ["Paz", "Pzt", "Sal", "Çarş", "Perş", "Cum", "Cmt", "Paz"],
+    monthsShort: [
+      "Ocak",
+      "Şubat",
+      "Mart",
+      "Nisan",
+      "Mayıs",
+      "Haziran",
+      "Temmuz",
+      "Ağustos",
+      "Eylül",
+      "Ekim",
+      "Kasım",
+      "Aralık",
+    ],
+    meridiem: ["am", "pm"],
+    suffix: ["st", "nd", "rd", "th"],
+    todayBtn: "Bugün",
+    clearBtn: "Temizle",
+    timeView: "Show time view",
+    backToDate: "Back to calendar view",
+  };
+  let schema = yup.object().shape({
+    name: yup.string().required("Lütfen adınızı giriniz"),
+    surname: yup.string().required("Lütfen soyadınızı giriniz"),
+    email: yup
+      .string()
+      .required("Lütfen e-mail adresinizi girin")
+      .email("Lütfen geçerli bir e-mail adresi girin"),
+    phone: yup
+      .string()
+      .matches(
+        /^\d{10}$/,
+        "Lütfen başında 0 olmadan 10 haneli bir telefon numarası girin"
+      ),
+    birthdate: yup.string().required("Lütfen doğum tarihinizi girin"),
+    password: yup
+      .string()
+      .required("Lütfen şifrenizi girin")
+      .min(8, "Geçersiz şifre. Şifreniz en az 8 karakter olmalıdır"),
+  });
+  async function validate() {
+    try {
+      await schema.validate(user, { abortEarly: false });
+      console.log(user);
+      user = {
+        name: "",
+        surname: "",
+        email: "",
+        phone: "",
+        birthdate: "",
+        password: "",
+      };
+      errors = {
+        name: "",
+        surname: "",
+        email: "",
+        phone: "",
+        birthdate: "",
+        password: "",
+      };
+      toast.success("Kaydınız Başarıyla Oluşturuldu!", {
+        position: "top-right",
+      });
+    } catch (err) {
+      errors = err.inner.reduce((acc, err) => {
+        return { ...acc, [err.path]: err.message };
+      }, {});
+    }
   }
 </script>
 
-<!-- markup (zero or more items) goes here -->
+<Toaster />
 <div class="container-fluid">
   <div class="row">
     <div class="col-6"><SideImage /></div>
@@ -28,74 +133,111 @@
           <h1 class="text-center">Üye Ol</h1>
         </div>
       </div>
-      <div class="ml-page">
-        <div class="input-group mt-3 mb-3">
-          <Textfield class="" variant="filled" bind:value={name} label="İsim">
-            <Icon class="material-icons" slot="leadingIcon">person</Icon>
-          </Textfield>
-          <span class="me-3" />
-          <Textfield variant="filled" bind:value={surname} label="Soyisim">
-            <Icon class="material-icons" slot="leadingIcon">person</Icon>
-          </Textfield>
-        </div>
+      <form on:submit|preventDefault={validate}>
+        <div class="ml-page">
+          <div class="input-group mt-3 mb-2">
+            <Textfield
+              style="width: 505px;"
+              variant="outlined"
+              bind:value={user.name}
+              label="İsim"
+            >
+              <Icon class="material-icons" slot="leadingIcon">person</Icon>
+            </Textfield>
+            <small class="invalid-feedback d-block"
+              >{#if errors.name}{errors.name}{/if}</small
+            >
+          </div>
+          <div class="input-group mb-2">
+            <Textfield
+              style="width: 505px;"
+              variant="outlined"
+              bind:value={user.surname}
+              label="Soyisim"
+            >
+              <Icon class="material-icons" slot="leadingIcon">person</Icon>
+            </Textfield>
+            <small class="invalid-feedback d-block"
+              >{#if errors.surname}{errors.surname}{/if}</small
+            >
+          </div>
+          <div class="input-group mb-2">
+            <Textfield
+              style="width: 505px;"
+              type="email"
+              variant="outlined"
+              bind:value={user.email}
+              label="E-posta"
+            >
+              <Icon class="material-icons" slot="leadingIcon">email</Icon>
+            </Textfield>
+            <small class="invalid-feedback d-block"
+              >{#if errors.email}{errors.email}{/if}</small
+            >
+          </div>
+          <div class="input-group mb-2">
+            <Textfield
+              style="width: 505px;"
+              type="phone"
+              variant="outlined"
+              prefix="0"
+              bind:value={user.phone}
+              label="Telefon Numarası"
+            >
+              <Icon class="material-icons" slot="leadingIcon">phone</Icon>
+            </Textfield>
+            <small class="invalid-feedback d-block"
+              >{#if errors.phone}{errors.phone}{/if}</small
+            >
+          </div>
+          <div class="input-group date-picker mb-2">
+            <SveltyPicker
+              i18n={tr}
+              placeholder={"Doğum Tarihi"}
+              inputClasses="form-control"
+              format="dd/mm/yyyy"
+              bind:value={user.birthdate}
+            />
 
-        <div class="input-group mb-3">
-          <Textfield
-            style="width: 505px;"
-            type="email"
-            variant="filled"
-            bind:value={email}
-            label="E-posta"
-          >
-            <Icon class="material-icons" slot="leadingIcon">email</Icon>
-          </Textfield>
-        </div>
-        <div class="input-group mb-3 ">
-          <Textfield
-            style="width: 505px;"
-            type="phone"
-            variant="filled"
-            bind:value={phone}
-            label="Telefon Numarası"
-          >
-            <Icon class="material-icons" slot="leadingIcon">phone</Icon>
-          </Textfield>
-        </div>
-        <div class="input-group mb-3 date-picker">
-          <SveltyPicker
-            placeholder={"Doğum Tarihi"}
-            inputClasses="form-control"
-            format="yyyy-mm-dd"
-            bind:value={birth}
-          />
-        </div>
-        <div class="input-group mb-3">
-          <Textfield
-            style="width: 505px;"
-            type="password"
-            class=""
-            variant="filled"
-            bind:value={password}
-            label="Şifre"
-          >
-            <Icon class="material-icons" slot="leadingIcon">password</Icon>
-          </Textfield>
-        </div>
-        <div class="row">
-          <div class="col d-flex justify-content-center ms-button ">
-            <Button class="bg-dark" on:click={submitForm} variant="raised">
-              <Label>Kayıt ol</Label>
-            </Button>
+            <small class="invalid-feedback d-block">
+              {#if errors.birthdate}{errors.birthdate}{/if}</small
+            >
+          </div>
+          <div class="input-group mb-3">
+            <Textfield
+              style="width: 505px;"
+              type="password"
+              variant="outlined"
+              bind:value={user.password}
+              label="Şifre"
+            >
+              <Icon class="material-icons" slot="leadingIcon">password</Icon>
+            </Textfield>
+            <small class="invalid-feedback d-block"
+              >{#if errors.password}{errors.password}{/if}</small
+            >
+          </div>
+          <div class="row">
+            <div class="col d-flex justify-content-center ms-button ">
+              <Button
+                style="text-transform: none;"
+                class="bg-dark"
+                type="submit"
+                variant="raised"
+              >
+                <Label>Kayıt ol</Label>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </div>
 
 <style>
   .mt-page {
-    margin-top: 300px;
+    margin-top: 180px;
   }
 
   .ms-button {
@@ -107,5 +249,8 @@
   .date-picker {
     width: 505px;
     height: 56px;
+  }
+  .invalid-feedback {
+    color: red;
   }
 </style>
