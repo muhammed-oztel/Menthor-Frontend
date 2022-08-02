2bfb7e552a6e9529d9f8ed091ead6ac2a142952a
Zeynep Afat <zynpaft19@gmail.com>
2022-08-02 18:38:55 +0300

    özellikler kısmı düzeltildi, profilde routelar tekrar yazıldı

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -21,10 +21,7 @@
                         
                     </div>
                 </div>
-                
-                 
-               
-              </div>
+               </div>
             </div>
           </div>
         </div>
--- src/pages/Specs.svelte
+++ src/pages/Specs.svelte
@@ -1,37 +1,29 @@
 <script lang="ts">
-   
-                               
-                            
-</script>
+ </script>
 
-<main>
+
     <main class="main">
         <div class="container" >
-            <div class="row align-items-center vh-100">
+           
               <div class="col-8 mx-auto">
-                
-                   
                 <div class="card card-rounded shadow border-0">
                   <div class="card-body d-flex flex-column  "> 
-                    <h1 class="card-title mb-2">Özellikler</h1>
+                    <h1 class="card-title mb-2">Özelliklerimiz</h1>
                     <hr>
-        
-                    <div class="row mb-4">
-                        <div class="col-12">
-                            <div class="row row-cols-1 row-cols-md-2 g-4">
+                        <div class="row row-cols-1 row-cols-md-2 g-4">
 
                                 <div class="col">
                                   <div class="card border-dark mb-3">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="https://media.istockphoto.com/vectors/free-3d-speech-bubble-sign-vector-id1156404289?k=20&m=1156404289&s=612x612&w=0&h=jpzVSuwyf8D33nng1wyGe80GRj9B5r7RvWA2_rf2bvc=" class="card-img-top" alt="...">
                                     <div class="card-body">
                                       <h5 class="card-title">Tamamen Ücretsiz</h5>
-                                      <p class="card-text">%100 ücretsiz olan bu platformda alanında uzman menteelerle görüşebilirsiniz.</p>
+                                      <p class="card-text">%100 ücretsiz olan bu platformda alanında uzman mentorlarımızla görüşebilirsiniz.</p>
                                     </div>
                                   </div>
                                 </div>
                                 <div class="col">
                                   <div class="card border-dark mb-3">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="https://blog.hubspot.com/hubfs/characteristics-of-professional-salespeople.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
                                       <h5 class="card-title">Alanında Uzman Mentorlar</h5>
                                       <p class="card-text">Özel olarak seçtiğimiz uzmanlarımızla kariyer yolculuğunuza eşlik edelim.</p>
@@ -40,7 +32,7 @@
                                 </div>
                                 <div class="col">
                                   <div class="card border-dark mb-3">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="https://cdn.vox-cdn.com/thumbor/WGFkB4VXayGONz6k9Zaz5OTTEW8=/0x0:1024x652/1200x800/filters:focal(431x245:593x407)/cdn.vox-cdn.com/uploads/chorus_image/image/63362453/GVC_UI_50people_laptop_1024x652.0.png" class="card-img-top" alt="...">
                                     <div class="card-body">
                                       <h5 class="card-title">Görüntülü Görüşebilirsiniz</h5>
                                       <p class="card-text">Eklediğimiz platform sayesinde görüntülü görüştüğünüz mentorlarımızla hiçbir zaman yalnız değilsiniz.</p>
@@ -49,33 +41,27 @@
                                 </div>
                                 <div class="col">
                                   <div class="card border-dark mb-3">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="https://static.vecteezy.com/system/resources/thumbnails/004/685/169/small/flat-style-design-illustration-icon-creative-technology-education-library-easy-access-information-concept-touch-screen-pad-ebook-library-books-map-pin-vector.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
                                       <h5 class="card-title">Kolay Kullanım</h5>
                                       <p class="card-text">Tasarladığımız arayüz sayesinde içeriklerinize kolayca erişim imkanı </p>
                                     </div>
                                   </div>
                                 </div>
-                              </div>
-                            
+                            </div>
+                         </div>
                         </div>
                     </div>
-                    
-                     
-                   
-                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-    
-    </main>
-</main>
+              </main>
+
 
 <style>
+   
     .main{
-        background-image: url("https://www.weebly.com/uploads/1/_/1/3261703_orig.jpg");
-        background-repeat: no-repeat;
-        background-size: 100%;
+        background-size: auto;
+        background-color: black;
+        width: 100vw;
+        min-height: 100vh;
     }
 </style>
