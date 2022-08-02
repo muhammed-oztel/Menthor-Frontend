32b6fe38da5692db5cc010a76801a6949e0abd05
Zeynep Afat <zynpaft19@gmail.com>
2022-08-02 15:39:28 +0300

    about ve özellikler kısmı eklendi

--- src/pages/Specs.svelte
+++ src/pages/Specs.svelte
@@ -1,9 +1,81 @@
-<script>
+<script lang="ts">
+   
+                               
+                            
 </script>
 
 <main>
-    <h1>Özellikler</h1>
+    <main class="main">
+        <div class="container" >
+            <div class="row align-items-center vh-100">
+              <div class="col-8 mx-auto">
+                
+                   
+                <div class="card card-rounded shadow border-0">
+                  <div class="card-body d-flex flex-column  "> 
+                    <h1 class="card-title mb-2">Özellikler</h1>
+                    <hr>
+        
+                    <div class="row mb-4">
+                        <div class="col-12">
+                            <div class="row row-cols-1 row-cols-md-2 g-4">
+
+                                <div class="col">
+                                  <div class="card border-dark mb-3">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Tamamen Ücretsiz</h5>
+                                      <p class="card-text">%100 ücretsiz olan bu platformda alanında uzman menteelerle görüşebilirsiniz.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card border-dark mb-3">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Alanında Uzman Mentorlar</h5>
+                                      <p class="card-text">Özel olarak seçtiğimiz uzmanlarımızla kariyer yolculuğunuza eşlik edelim.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card border-dark mb-3">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Görüntülü Görüşebilirsiniz</h5>
+                                      <p class="card-text">Eklediğimiz platform sayesinde görüntülü görüştüğünüz mentorlarımızla hiçbir zaman yalnız değilsiniz.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card border-dark mb-3">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Kolay Kullanım</h5>
+                                      <p class="card-text">Tasarladığımız arayüz sayesinde içeriklerinize kolayca erişim imkanı </p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            
+                        </div>
+                    </div>
+                    
+                     
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    
+    </main>
 </main>
 
 <style>
+    .main{
+        background-image: url("https://www.weebly.com/uploads/1/_/1/3261703_orig.jpg");
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
 </style>
