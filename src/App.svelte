93d7d2d9bdf0f8bbaa52874c68ee2ed28d83c6ed
melihcelik <melihcelik0909@gmail.com>
2022-08-04 14:36:20 +0300

    Dosya UI ve Anasayfa Düzeltildi

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import { Router, Route } from "svelte-routing";
-  import Files from "./pages/Files.svelte";
   import LoginPage from "./pages/LoginPage.svelte";
   import NotFound from "./pages/NotFound.svelte";
   import Profile from "./pages/Profile.svelte";
@@ -17,11 +16,10 @@
   <Route path="profil" component={Profile} />
   <Route path="panel" component={Panel} />
   <Route path="takvim" component={Calendar} />
-  <Route path="dosyalar" component={Files} />
   <Route path="ayarlar" component={Settings} />
   <Route path="giris" component={LoginPage} />
   <Route path="kayit" component={RegisterPage} />
-  <Route path="folder" component={Folder} />
+  <Route path="dosyalar" component={Folder} />
   <Route path="/" component={Home} />
   <Route path="*" component={NotFound} />
   <Route path="videocall" component={VideoCall} />
--- src/pages/Folder.svelte
+++ src/pages/Folder.svelte
@@ -2,28 +2,31 @@
     import { uploadFiles } from "../services/file.js";
     import toast, { Toaster } from "svelte-french-toast";
     import { navigate } from "svelte-routing";
-    import  { Label} from '@smui/button';
-    import Fab from '@smui/fab';
+    import { Label } from "@smui/button";
+    import Fab from "@smui/fab";
 
     let files;
-    //senin ben mkdir
     let list = [];
     let downloadURL;
     $: downloadURL;
     $: list = files;
-
+    let deleteFile = (dosyaAdi) => {
+        dosyalar = dosyalar.filter((dosya) => dosya.dosyaAdi != dosyaAdi);
+    };
     async function submitForm() {
         try {
             let upload = files[0];
             const formData = new FormData();
             formData.append("file", upload);
+            console.log(upload);
             if (upload) {
+                document.getElementById("upload-files").value = "";
                 uploadFiles(formData).then((res) => {
                     toast.success("Dosya başarıyla yüklendi.");
                     downloadURL = res.downloadURL;
                 });
             } else {
-                toast.error("Lütfen bir resim seçiniz.");
+                toast.error("Lütfen bir dosya seçiniz.");
             }
         } catch (error) {
             console.log(error);
@@ -46,52 +49,63 @@
 
 <Toaster />
 
-
-<div class="container">
-    <div class="row align-items-center vh-100">
-      <div class="col-8 mx-auto">
-        
-           
-        <div class="card card-rounded shadow border-0">
-          <div class="card-body d-flex flex-column  "> 
-            <h2 class="card-title mb-2">Dosyalar</h2>
-            <hr>
-
-            <div class="row mb-4">
-                <div class="col-12">
-                    <h4>Dosya Yükle</h4>
-                    <div class="form-group d-flex">
-                    <form on:submit|preventDefault={submitForm}>
-                    <input type="file" bind:files />
-                    <br> <br>
-                        <Fab color="primary"  extended>
-                            <Label>Seçilen Dosyayı Yükle</Label>
-                          </Fab>  
-                    </form>
-                    </div>     
-                </div>     
+<div class="container d-flex align-items-center justify-content-center">
+    <div class="card text-start">
+        <div class="card-body">
+            <h4 class="card-title mb-3">Dosya Yükleme</h4>
+            <form on:submit|preventDefault={submitForm}>
+                <input
+                    id="upload-files"
+                    bind:files
+                    type="file"
+                    class="form-control mb-3"
+                />
+                <button type="submit" class="btn btn-dark mb-3">
+                    Dosyayı Yükle
+                </button>
+            </form>
+            <hr />
+            <div>
+                <h4>Yüklenen Dosyalar Listesi</h4>
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">Dosya İsmi</th>
+                            <th scope="col">Yüklenme Tarihi</th>
+                            <th scope="col">Yüklenme Saati</th>
+                            <th scope="col">Dosya Silme</th>
+                            <th scope="col">Dosya İndirme</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Yeni Metin Belgesi.txt</td>
+                            <td>04.08.22</td>
+                            <td>12:00</td>
+                            <td><i class="bi bi-trash" /></td>
+                            <td><i class="bi bi-download" /></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            {#if list}
-                 {#each list as file}
-                    <div class="d-flex">
-                     <p class="me-3">{file.name}</p>
-                    <button on:click={downloadFile}>
-                    <i class="bi bi-download" />
-            </button>
         </div>
-        {/each}
-            {:else}
-            <p>Dosya Yok</p>
-        {/if}
-              
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 
-
-
-
+<!-- {#if list}
+                        {#each list as file}
+                            <div class="d-flex">
+                                <p class="me-3">{file.name}</p>
+                                <button on:click={downloadFile}>
+                                    <i class="bi bi-download" />
+                                </button>
+                            </div>
+                        {/each}
+                    {:else}
+                        <p>Dosya Yok</p>
+                    {/if} -->
 <style>
+    .container {
+        height: 100%;
+    }
 </style>
