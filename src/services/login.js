8f9112f81b4118787c8be7b0a93eeee78e0f1201
melihcelik <melihcelik0909@gmail.com>
2022-08-06 19:31:38 +0300

    Token Local Storage'a Eklendi

--- src/services/login.js
+++ src/services/login.js
@@ -3,6 +3,7 @@ import Api from "./Api";
 export const postLogin = async ( request ) => {
     try {
       const response = await Api.post("menthor/login", request);
+      localStorage.setItem("token", response.token);
       console.log("Send the request");
       return response;
     } catch (error) {
